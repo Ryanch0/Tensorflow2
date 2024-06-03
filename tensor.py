344c280110abf88f3a54ac0cf1e3d1bc4b56f559
Ryan <tjddnfkdls0@naver.com>
2024-06-03 22:33:27 +0900

    train

--- tensor.py
+++ tensor.py
@@ -1,17 +1,35 @@
 import tensorflow as tf
 
-tensor = tf.constant([3,4,5])
-tensor2 = tf.constant([3,4,5])
-tensor3 = tf.constant([[1,2],
-                       [3,4]])
-tensor4 = tf.constant([[5,2],
-                       [3,4]])
-print(tf.matmul(tensor3,tensor4))
+# height = 170
+# shoes = 260
+# # shoes = height*a + b
+
+# a = tf.Variable(0.1)
+# b = tf.Variable(0.2)
+
+# def 손실함수():
+#     예측값 = height*a + b
+#     return tf.square(260 - 예측값 )
+# # 경사하강법
+# opt = tf.keras.optimizers.legacy.Adam(learning_rate= 0.01)
+
+# for i in range(300):
+#     opt.minimize(손실함수, var_list=[a,b])
+#     print(a.numpy(),b.numpy())
+
+train_x = [1,2,3,4,5,6,7]
+train_y = [3,5,7,9,11,13,15]
+
+a = tf.Variable(0.1)
+b = tf.Variable(0.1)
 
 
-tensor6 = tf.zeros([2,2])
-print(tensor4.shape)
+def 손실함수(a,b):
+    예측_y = train_x * a +b
+    return tf.keras.losses.mse(train_y, 예측_y)
 
-# weight Variable
-w = tf.Variable(1.0) 
-print(w)
+opt = tf.keras.optimizers.legacy.Adam(learning_rate= 0.1)
+
+for i in range(1000):
+    opt.minimize(lambda:손실함수(a,b), var_list=[a,b])
+    print(a.numpy(),b.numpy())

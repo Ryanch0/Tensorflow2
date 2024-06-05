b07dfc2b505fba7a782f8abd75d44140a0581acb
Ryan <tjddnfkdls0@naver.com>
2024-06-05 17:49:52 +0900

    convolution layer

--- imgmatch.py
+++ imgmatch.py
@@ -1,19 +1,35 @@
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 
 (trainX, trainY), (testX, testY) = tf.keras.datasets.fashion_mnist.load_data()
 
 # print(trainX.shape)
 
+# 0~255를 0~1로 압축(선택사항)
+trainX = trainX / 255.0
+testX = testX / 255.0
+
+# 전처리하기
+# 원래 데이터는 (60000,28,28) 이었는데 쉐입 바꾸기 -> (60000,28,28,1)전체 데이터에 괄호쳐달라는의미
+trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+testX = testX.reshape((testX.shape[0], 28, 28, 1))
+
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankleboot']
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, input_shape=(28,28), activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
+    # 컨볼루션 레이어 Conv2D(n개의 이미지 복사본, (n,n 커널 사이즈),etc, activation='relu',input_shape)
+    # 이미지엔 음수가 없기에 relu 자주사용
+    tf.keras.layers.Conv2D(32,(3,3), padding="same", activation='relu', input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(64,(3,3), padding="same", activation='relu', input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    # tf.keras.layers.Dense(128, input_shape=(28,28), activation='relu'),
 
     # 행렬을 1차원으로 압축
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
     # 마지막 레이어 노드수를 카테고리 갯수만큼해서 확률예측하기
     tf.keras.layers.Dense(10, activation='softmax'),
 ])
@@ -21,6 +37,10 @@ model = tf.keras.Sequential([
 # summary 보려면 input_shape 넣어야함
 model.summary()
 
+optimizier = tf.keras.optimizers.legacy.Adam(learning_rate = 0.001)
+model.compile(loss = "sparse_categorical_crossentropy", optimizer=optimizier, metrics=['accuracy'])
+model.fit(trainX, trainY, validation_data=(testX,testY) ,epochs=5)
 
-model.compile(loss = "sparse_categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
-model.fit(trainX, trainY, epochs=5)
+# 학습 후 모델평가(학습용데이터가 아닌)
+score = model.evaluate(testX,testY)
+print(score)

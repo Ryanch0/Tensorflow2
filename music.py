0e79e6c2b21628be9e5a17bca619c4da227c1a19
Ryan <tjddnfkdls0@naver.com>
2024-06-09 18:57:28 +0900

    LSTM

--- music.py
+++ music.py
@@ -23,5 +23,31 @@ for i in text:
     numText.append(text_to_num[i])
 # print(numText)
 
+# X,Y데이터셋
 X = []
 Y = []
+
+#    이 전체가 반복문돌면서 sequence data training 가능
+for i in range(0, len(numText) -25 ):
+   X.append(numText[i : i + 25]) #i = 0일때 0~24까지를 X데이터로
+   Y.append(numText[i + 25]) #i = 0일때 25데이터를 Y데이터로
+
+# print(X[0 : 5])
+# print(Y[0  :5])
+
+# 원핫인코딩 왜 31이냐? 유니크문자의 갯수가 0부터 30까지 총 31개였음
+X = tf.one_hot(X, 31)
+Y = tf.one_hot(Y, 31)
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.LSTM(100, input_shape=(25,31)),
+    tf.keras.layers.Dense(31, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# LSTM은 epochs가 많이필요 100회이상..
+model.fit(X,Y, batch_size=64, epochs=100)
+
+model.save('music/models')
